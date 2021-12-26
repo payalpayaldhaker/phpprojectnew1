e27bed516152694f2e338269926df3d824af56a0
payalpayaldhaker <90671274+payalpayaldhaker@users.noreply.github.com>
2021-12-26 15:18:09 -0800

    new project in php learn baic 1

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>phpprojectnew 1</title>
+
+    <style>
+        h1{ 
+            color: red;
+        }
+
+    </style>
+</head>
+
+<body>
+    <?php  
+        $name ='payal dhakkad hello'.' '.'dhakakd sab '.' so smart work in morning';
+        echo $name;
+
+?>
+<?php  
+        echo '<h1>hello how are you payal dhakad</h1>';
+        //variable ka name number se start nhi hoga 
+        $a =10;
+        echo $a;
+?>
+</body>
+
+</html>
